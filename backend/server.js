4dbf0437b2569176f2e0c2f45fcd6bfe92b3169a
millonigada <milloni.gada251@gmail.com>
2024-03-30 17:06:34 -0700

    Fixing ENONT

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
 
     res.sendFile(
         path.join(__dirname, "../frontend/build/index.html"),
