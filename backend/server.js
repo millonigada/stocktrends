8f64a9f718326634cac3ead1514c9ae9c60ea696
millonigada <milloni.gada251@gmail.com>
2024-03-30 18:34:17 -0700

    Deployment -6

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.static(buildPath))
 app.get("/*", function (req, res){
 
     res.sendFile(
-        path.join(__dirname, "../frontend/public/index.html"),
+        path.join(__dirname, "../frontend/build/index.html"),
         function (err) {
             if (err) {
                 res.status(500).send(err)
