e61c7abe0e33d358c7acaa8e3634df4fc7d846b7
millonigada <milloni.gada251@gmail.com>
2024-03-31 17:51:43 -0700

    bug fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { AppContextProvider } from './context/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const serverURI = 'http://3.133.79.62/api/';
+const serverURI = 'http://3.133.79.62/api/';
 // const serverURI = '/api/';
-const serverURI = 'http://localhost:4000/api/';
+// const serverURI = 'http://localhost:4000/api/';
 export default serverURI;
 
 root.render(
